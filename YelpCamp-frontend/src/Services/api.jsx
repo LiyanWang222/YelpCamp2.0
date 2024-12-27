c1bfb649e2865bf63eb0981ae9f44c95ea618d36
Liyan Wang <liyanw@usc.edu>
2024-12-27 15:59:11 -0800

    test12

--- YelpCamp-frontend/src/Services/api.jsx
+++ YelpCamp-frontend/src/Services/api.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+axios.defaults.withCredentials = true;
 const api = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL, // 从环境变量读取 后端 API 基础 URL
     timeout: 100000, // 请求超时设置
